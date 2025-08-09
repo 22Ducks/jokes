88cf121633307f7d2f8f5682c53389925bcd2a34
jessi <jessienduck@gmail.com>
2025-08-09 14:02:40 -0400

    Modified code to personalize the look of the site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+const jokesApiBase = "https://sv443.net/jokeapi/v2/";
+
+const options = {
+  method: 'GET', // Explicitly setting the method, though 'GET' is default
+  headers: {
+    'Content-Type': 'application/json', // Example: Content-Type header
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [joke, setJoke] = useState("");
+
+  const getJoke = async () => {
+    try {
+      const response = await fetch(jokesApiBase, options);
+      if(!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setJoke(result);
+    } catch (error) {
+      console.error("Caught an unknown error.");
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="/congrationYouDoneIt.png" className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Flock of Jokes</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={getJoke}>Get Joke</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <p>{joke}</p>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
